4aa4e60133f5b4bcf9cea0f3f682589c254c640b
Malte Nottmeyer <nottmey@users.noreply.github.com>
2022-05-13 13:12:13 +0200

    add gen tests

--- src/datomic_lacinia/testing.clj
+++ src/datomic_lacinia/testing.clj
@@ -1,7 +1,9 @@
 (ns datomic-lacinia.testing
   (:require [datomic.client.api :as d]
             [com.walmartlabs.lacinia :as l]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.test :refer [deftest- is]]
+            [clojure.walk :as w]))
 
 (defn local-temp-conn
   ([] (local-temp-conn "testing"))
@@ -13,6 +15,20 @@
          _        (d/delete-database client database)
          _        (d/create-database client database)]
      (d/connect client database))))
+
+(defn clean [nested-maps remove-keys]
+  (w/prewalk
+    (fn [node]
+      (if (map? node)
+        (apply dissoc node remove-keys)
+        node))
+    nested-maps))
+
+(deftest- clean-test
+  (is (= (clean [] [:x]) []))
+  (is (= (clean [:a :x] [:x]) [:a :x]))
+  (is (= (clean {:x 1} [:x]) {}))
+  (is (= (clean {:a {:x 1}, :x 2} [:x]) {:a {}})))
 
 (defn execute [schema query-file variables]
   (l/execute schema (slurp (io/resource query-file)) variables nil))
