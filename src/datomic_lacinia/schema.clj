2446df3208b575b4f90cda516533bdfb387cfcb4
Malte Nottmeyer <nottmey@users.noreply.github.com>
2022-05-13 13:12:13 +0200

    add gen tests

--- src/datomic_lacinia/schema.clj
+++ src/datomic_lacinia/schema.clj
@@ -88,8 +88,22 @@
               (rest remaining-paths))))
         objects))))
 
-(comment
-  (gen-result-objects (datomic/attributes (datomic/current-db)) :Entity)
+(deftest- gen-result-objects-test
+  (let [objects (gen-result-objects [datomic/default-id-attribute] :Entity)]
+    (is (= (testing/clean objects [:resolve])
+           {:Entity   {:description "An entity of this application.",
+                       :fields      {:db {:type        :EntityDb,
+                                          :description "Nested data :db as type :EntityDb"}}},
+            ; TODO better naming 'DbContext'
+            :EntityDb {:description "Nested data of attribute :db on type :Entity",
+                       :fields      {:id    {:type         :ID,
+                                             :db/attribute :db/id,
+                                             :db/type      :db.type/long,
+                                             :description  "Attribute used to uniquely identify an entity, managed by Datomic."},
+                                     :ident {:type         :String,
+                                             :db/attribute :db/ident,
+                                             :db/type      :db.type/keyword,
+                                             :description  "Attribute used to uniquely name an entity."}}}})))
 
   ;; checking for backref
   (let [schema-with-refs [#:db{:ident       :artist/name,
@@ -105,8 +119,50 @@
                           #:db{:ident       :track/influencers
                                :valueType   #:db{:ident :db.type/ref}
                                :cardinality #:db{:ident :db.cardinality/many}
-                               :doc         "Artists who had influences on the style of this track"}]]
-    (gen-result-objects schema-with-refs :Entity)))
+                               :doc         "Artists who had influences on the style of this track"}]
+        objects          (gen-result-objects schema-with-refs :Entity)]
+    (is (= (testing/clean objects [:resolve])
+           {:Entity                  {:description "An entity of this application.",
+                                      :fields      {:db           {:type :EntityDb, :description "Nested data :db as type :EntityDb"},
+                                                    :artist       {:type :EntityArtist, :description "Nested data :artist as type :EntityArtist"},
+                                                    :track        {:type :EntityTrack, :description "Nested data :track as type :EntityTrack"},
+                                                    ;; TODO referencedBy_
+                                                    :referencedBy {:type        :EntityReferencedBy,
+                                                                   :description "Nested data :referencedBy as type :EntityReferencedBy"}}},
+            :EntityDb                {:description "Nested data of attribute :db on type :Entity",
+                                      :fields      {:id    {:type         :ID,
+                                                            :db/attribute :db/id,
+                                                            :db/type      :db.type/long,
+                                                            :description  "Attribute used to uniquely identify an entity, managed by Datomic."},
+                                                    :ident {:type         :String,
+                                                            :db/attribute :db/ident,
+                                                            :db/type      :db.type/keyword,
+                                                            :description  "Attribute used to uniquely name an entity."}}},
+            :EntityArtist            {:description "Nested data of attribute :artist on type :Entity",
+                                      :fields      {:name {:type :String, :db/attribute :artist/name, :db/type :db.type/string}}},
+            :EntityTrack             {:description "Nested data of attribute :track on type :Entity",
+                                      :fields      {:name        {:type :String, :db/attribute :track/name, :db/type :db.type/string},
+                                                    :artists     {:type         '(list :Entity),
+                                                                  :db/attribute :track/artists,
+                                                                  :db/type      :db.type/ref,
+                                                                  :description  "Artists who contributed to the track"},
+                                                    :influencers {:type         '(list :Entity),
+                                                                  :db/attribute :track/influencers,
+                                                                  :db/type      :db.type/ref,
+                                                                  :description  "Artists who had influences on the style of this track"}}},
+            ; TODO ReferencedByContext
+            :EntityReferencedBy      {:description "Nested data of attribute :referencedBy on type :Entity",
+                                      :fields      {:track {:type        :EntityReferencedByTrack,
+                                                            :description "Nested data :track as type :EntityReferencedByTrack"}}},
+            :EntityReferencedByTrack {:description "Nested data of attribute :track on type :EntityReferencedBy",
+                                      :fields      {:artists     {:type         '(list :Entity),
+                                                                  :db/attribute :track/_artists,
+                                                                  :db/type      :db.type/ref,
+                                                                  :description  "Holds all entities which referencing via :track/artists"},
+                                                    :influencers {:type         '(list :Entity),
+                                                                  :db/attribute :track/_influencers,
+                                                                  :db/type      :db.type/ref,
+                                                                  :description  "Holds all entities which referencing via :track/influencers"}}}}))))
 
 (defn gen-input-objects [result-objects]
   (let [ref->input-ref           (fn [k] (if (get result-objects k) (graphql/input-type-key k) k))
